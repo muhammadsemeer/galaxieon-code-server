fd16833235a01149b7bb7f77ef4bca6701108d51
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-06-08 10:55:48 +0530

    database connection modified

--- config/dbconnection.ts
+++ config/dbconnection.ts
@@ -1,6 +1,27 @@
 import { Sequelize } from "sequelize";
 import { dbConfig } from "../constants/db";
+import { readdirSync } from 'fs'
+import { join } from "path";
 
-const db = new Sequelize({...dbConfig, dialect: "mariadb"});
+const sequelize: Sequelize = new Sequelize({ ...dbConfig, dialect: "mariadb" });
 
-export default db;
+const db:any = {};
+const models = join(__dirname,"../", "models"); // path to a models' folder
+
+readdirSync(models)
+  .filter(function (file) {
+    return file.indexOf(".") !== 0 && file.slice(-3) === ".ts";
+  })
+  .forEach(function (file) {
+    import (join(models, file)).then((value) => {
+        db[value.User.name] = value.User;
+    });
+  });
+  
+Object.keys(db).forEach(function (modelName) {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+export default sequelize;
