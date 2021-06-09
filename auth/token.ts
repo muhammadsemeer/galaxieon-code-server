0e811ccf8854fa7bebec9306817c6f6d446103a7
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-06-09 14:44:34 +0530

    added verifyTokenFunctions:

--- auth/token.ts
+++ auth/token.ts
@@ -1,6 +1,7 @@
-import { User } from "../types/User";
-import { sign } from "jsonwebtoken";
+import { RequestWithUser, User } from "../types/User";
+import { sign, verify } from "jsonwebtoken";
 import { config } from "dotenv";
+import { Response, NextFunction } from "express";
 config();
 
 export const createToken = (
@@ -23,4 +24,76 @@ export const createToken = (
   );
 
   return { accessToken, refreshToken };
+};
+
+export const verifyToken = (
+  req: RequestWithUser | any,
+  res: Response,
+  next: NextFunction
+) => {
+  let accessToken: string = req.cookies.accessToken,
+    refreshToken: string = req.cookies.refreshToken;
+  if (accessToken && refreshToken) {
+    verifyAccessToken(accessToken)
+      .then((result) => {
+        req.user = result;
+        return next();
+      })
+      .catch((err) => {
+        verifyRefreshToken(refreshToken)
+          .then((result) => {
+            createAccessToken(result, req, res, next);
+          })
+          .catch((err) => {
+            next({ message: "User not Authentictaed", status: 403 });
+          });
+      });
+  } else if (!accessToken && refreshToken) {
+    verifyRefreshToken(refreshToken)
+      .then((result) => {
+        createAccessToken(result, req, res, next);
+      })
+      .catch((err) => {
+        next({ message: "User not Authentictaed", status: 403 });
+      });
+  } else {
+    next({ message: "User not Authentictaed", status: 403 });
+  }
+};
+
+function verifyAccessToken(token: string): Promise<Object | undefined> {
+  return new Promise((resolve, reject) => {
+    verify(token, process.env.JWT_SECRET as string, (err, decoded) => {
+      if (err) return reject(err);
+      resolve(decoded);
+    });
+  });
+}
+
+function verifyRefreshToken(token: string): Promise<Object | undefined> {
+  return new Promise((resolve, reject) => {
+    verify(token, process.env.JWT_REFRESH_TOKEN as string, (err, decoded) => {
+      if (err) return reject(err);
+      resolve(decoded);
+    });
+  });
+}
+
+export const createAccessToken = (
+  user: User | any,
+  req: RequestWithUser,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id, name, email }: User = user;
+  let accessToken = sign(
+    { id, name, email },
+    process.env.JWT_SECRET as string,
+    {
+      expiresIn: "1d",
+    }
+  );
+  req.user = user;
+  res.cookie("accessToken", accessToken);
+  return next();
 };
