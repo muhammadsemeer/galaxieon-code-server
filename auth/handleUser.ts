9867c91f5b427e00eaa29bbbd024d8de6efd024e
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-06-18 21:32:12 +0530

    adding authType on new User and checking is user blocked or not

--- auth/handleUser.ts
+++ auth/handleUser.ts
@@ -1,26 +1,31 @@
 import { TokenPayload } from "google-auth-library";
-import db from "../config/dbconnection";
 import { User } from "../types/User";
 import UserModel from "../models/User";
+import { Op } from "sequelize";
 
 export const createUserOrLogUser = async (
   payload:
     | TokenPayload
     | { name: string; email: string | null; picture: string },
+  type: "google" | "github",
   cb: Function
 ): Promise<void> => {
   try {
     const { name, email, picture } = payload;
     let code: number = 200;
     let isUser: User = await UserModel.findOne({
-      where: { email: payload.email, status: true },
-      attributes: ["id", "name", "email", "profileImage"],
+      where: {
+        email: payload.email,
+        [Op.or]: [{ status: "active" }, { status: "blocked" }],
+      },
+      attributes: ["id", "name", "email", "profileImage", "status"],
     });
     if (!isUser) {
       let created: User = await UserModel.create({
         name,
         email,
         profileImage: picture,
+        authType: type,
       });
       isUser = {
         id: created.id,
@@ -29,8 +34,12 @@ export const createUserOrLogUser = async (
         profileImage: created.profileImage,
       };
       code = 201;
+      cb(null, isUser, code);
+    } else if (isUser?.status === "blocked") {
+      cb({ status: 403, message: "User Blocked By ADMIN" });
+    } else {
+      cb(null, isUser, code);
     }
-    cb(null, isUser, code);
   } catch (error) {
     cb(error);
   }
