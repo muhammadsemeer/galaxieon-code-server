93687be7bafd8d47b3a8b2608a637fcc78b8b507
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-06-19 14:55:10 +0530

    created route for getting all users

--- routers/admin.ts
+++ routers/admin.ts
@@ -1,11 +1,20 @@
-import { Router, Request, Response, NextFunction } from "express";
+import { Router, Response, NextFunction } from "express";
 import { verifyAdmin } from "../auth/token";
 import { RequestWithAdmin } from "../types/User";
+import UserDetails from "../helpers/admin/user";
 
 const router: Router = Router();
 
 // Middleware for verfiying admin
 router.use(verifyAdmin);
 
+router.get(
+  "/users",
+  (req: RequestWithAdmin, res: Response, next: NextFunction) => {
+    UserDetails.getAllUsers()
+      .then((users) => res.json(users))
+      .catch((err) => next(err));
+  }
+);
 
 export default router;
