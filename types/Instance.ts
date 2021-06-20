d7475cdc819c679e3032e6ecd859f71bc0804a5c
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-06-20 12:36:33 +0530

    created instances types

---
+++ types/Instance.ts
@@ -0,0 +1,23 @@
+export type File = { name: string; files: string[] };
+
+export interface Files {
+  name: string;
+  files: string[];
+  folder: File[];
+}
+
+export interface Instance {
+  name: string;
+  description?: string;
+  keywords?: string;
+  isPriavte?: boolean;
+  fork?: boolean;
+  files: Files;
+  autosave?: boolean;
+  autopreview?: boolean;
+  likes?: number;
+  shares?: number;
+  forks?: number;
+  deletedAt?: Date;
+  status?: boolean;
+}
