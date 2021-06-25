093d7e0a674c49cb0787ac689ea70f24f02fca7b
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-06-25 19:19:47 +0530

    logging only development env

--- config/dbconnection.ts
+++ config/dbconnection.ts
@@ -5,6 +5,7 @@ const sequelize: Sequelize = new Sequelize({
   ...dbConfig,
   dialect: "mariadb",
   dialectOptions: { autoJsonMap: false },
+  logging: process.env.NODE_ENV === "development" ? true : false,
 });
 
 export default sequelize;
