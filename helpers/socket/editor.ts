f16f9c91d58a36a845433736b57df0caf6a4bd75
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-06-26 22:38:52 +0530

    updating code edited date on socket disconnect:

--- helpers/socket/editor.ts
+++ helpers/socket/editor.ts
@@ -1,10 +1,13 @@
 import { Namespace } from "socket.io";
 import { editCode } from "../instance/codeHandler";
+import { edited } from "../instance/handle";
 import { SocketWithCookies } from "./socket-cookie-parser";
 
 export const editorSocket = (io: Namespace, socket: SocketWithCookies) => {
+  var id: string;
   socket.on("join", (instanceId) => {
     socket.join(instanceId);
+    id = instanceId;
   });
   socket.on("change", (file, value, instanceId, cb) => {
     editCode(file, instanceId, value)
@@ -15,5 +18,13 @@ export const editorSocket = (io: Namespace, socket: SocketWithCookies) => {
       .catch((err) => {
         cb(err, "FAIL");
       });
+  });
+
+  socket.on("disconnect", async () => {
+    try {
+      await edited(id);
+    } catch (error) {
+      console.log(error);
+    }
   });
 };
