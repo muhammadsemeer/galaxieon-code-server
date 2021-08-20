fa57fa9d2945c6fb3f148b73077aced6a9b6cc9a
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-08-20 11:54:44 +0530

    checking is edited

--- helpers/socket/editor.ts
+++ helpers/socket/editor.ts
@@ -6,13 +6,16 @@ import { SocketWithCookies } from "./socket-cookie-parser";
 
 export const editorSocket = (io: Namespace, socket: SocketWithCookies) => {
   var id: string;
+  var isChangesMade: boolean;
   socket.on("join", (instanceId) => {
     socket.join(instanceId);
     id = instanceId;
+    console.log("Editor connected to instance: " + instanceId);
   });
   socket.on("change", (file, value, instanceId, cb) => {
     editCode(file, instanceId, value)
       .then(() => {
+        isChangesMade = true;
         socket.broadcast.emit("change_by_other", value);
         cb(null, "OK");
       })
@@ -22,14 +25,14 @@ export const editorSocket = (io: Namespace, socket: SocketWithCookies) => {
   });
 
   socket.on("execute", (file, instanceId) => {
-    compile(file, instanceId,socket, (err, result) => {
+    compile(file, instanceId, socket, (err, result) => {
       socket.emit("exe_result", err, result);
     });
   });
 
   socket.on("disconnect", async () => {
     try {
-      await edited(id);
+      isChangesMade && (await edited(id));
     } catch (error) {
       console.log(error);
     }
