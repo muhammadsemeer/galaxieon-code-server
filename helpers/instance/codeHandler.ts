49f3fbd13e92d63a2c82a66427bdc121395b6866
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-10-24 18:12:30 +0530

    authencted user can edit all users code issue fixed

--- helpers/instance/codeHandler.ts
+++ helpers/instance/codeHandler.ts
@@ -1,12 +1,18 @@
 import fs from "fs";
 import path from "path";
+import { getInstanceById } from "./handle";
 
 export const editCode = (
   fileName: string,
   instanceId: string,
+  userId: string,
   value: string
 ): Promise<void> => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
+    const instance = await getInstanceById(instanceId);
+    if (instance.UserId !== userId) {
+      reject("Forbidden");
+    }
     let src = path.join(
       __dirname,
       "../../public/instances",
