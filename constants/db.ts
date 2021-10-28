8565276a56940104003a2d2fed3f30ec3ef76201
Muhammad Semeer <69565787+muhammadsemeer@users.noreply.github.com>
2021-10-28 19:49:48 +0530

    Update db.ts

--- constants/db.ts
+++ constants/db.ts
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 config();
 
 export const dbConfig = {
-  database: "galaxieonCode",
+  database: process.env.DB_NAME as string,
   host: process.env.DB_HOST as string,
   username: process.env.DB_USER as string,
   password: process.env.DB_PASS as string,
